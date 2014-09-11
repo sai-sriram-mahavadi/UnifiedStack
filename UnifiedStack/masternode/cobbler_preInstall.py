c0839f736ceac1f9848148843732e17dd514c96b
Prakash24 <prakkum3@cisco.com>
2014-09-11 12:29:47 +0530

    Removed unnecessary errors

--- UnifiedStack/masternode/cobbler_preInstall.py
+++ UnifiedStack/masternode/cobbler_preInstall.py
@@ -12,8 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-#!/bin/sh
-# exec /share/vim/vim74/vim "$@"
+#!/bin/python
+
 
 # This File prepares system for cobbler installation.
 from general_utils import shell_command, bcolors

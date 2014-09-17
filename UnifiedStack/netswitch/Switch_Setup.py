6e86586de003339927bc2f0f6343f48d4bce4779
sudarsku <sudarsku@cisco.com>
2014-09-17 18:11:00 +0530

    1.Added the setup and configuration files for 3750,2960,Terminsl Server(TS) and N9k 2.The Switch_setup.py calls the individual switch configurators.

--- UnifiedStack/netswitch/Switch_Setup.py
+++ UnifiedStack/netswitch/Switch_Setup.py
@@ -1,76 +1,18 @@
-#   Copyright 2014
-#
-#   Licensed under the Apache License, Version 2.0 (the "License");
-#   you may not use this file except in compliance with the License.
-#   You may obtain a copy of the License at
-#
-#       http://www.apache.org/licenses/LICENSE-2.0
-#
-#   Unless required by applicable law or agreed to in writing, software
-#   distributed under the License is distributed on an "AS IS" BASIS,
-#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-#   See the License for the specific language governing permissions and
-#   limitations under the License.
-#   Establishes an ssh session to the switch.
-#   Configures the switch.
-
-import paramiko
-import time
-
-# TODO ip, username and password will be taken from config file later.
-ip = '10.106.16.253'
-username = 'sdu'
-password = '1@#$sDu%^7'
+import Switch2960_Setup as sw_2960
+import Switch3750_Setup as sw_3750
+import Switch9k_Setup as n9k
+import TS_Setup as TS
 
 
 class SwitchConfigurator:
-    # Create instance of SSHClient object
-    # Automatically add untrusted hosts (make sure okay for security policy
-    # in your environment)
-    # initiate SSH connection
-    # Use invoke_shell to establish an 'interactive session'
-    # remote_conn = remote_conn_pre.invoke_shell()
+	
+	def configure_switch(self)
+		sw3750_config = sw_3750.Switch3750Configurator()
+		sw3750_config.configure3750_switch()
+		sw9k_config = n9k.Switch9kConfigurator()
+		sw9k_config.configure9k_switch()
 
-    def establish_connection(self, ipaddress, username, password):
-        remote_conn_pre = paramiko.SSHClient()
-        remote_conn_pre.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-        remote_conn_pre.connect(
-            ipaddress,
-            username=username,
-            password=password)
-        print "SSH connection established to %s" % ipaddress
-        print "Interactive SSH session established"
-        return remote_conn_pre
-
-    def configure_switch(self):
-        # Calling the function to make the ssh connection
-        remote_conn_client = self.establish_connection(
-            ipaddress=ip,
-            username=username,
-            password=password)
-        # Use invoke_shell to establish an 'interactive session'
-        remote_conn = remote_conn_client.invoke_shell()
-        output = remote_conn.recv(1000)
-        print output
-        # sending configuration commands to switch from a text file as input
-        for line in open('sw_commands.txt'):
-            # error check for ssh connection and send function and retry 3
-            # times if fail
-            success = False
-            attempts = 0
-            while not success and attempts < 3
-                try:
-                    remote_conn.send(line)
-                    success = True
-                except socket.error as e:
-                    print "Connection is not established : " + e.strerror
-                    attempts += 1
-        # Adding a delay to let the commands work. Add at the end of all
-        # commands
-        time.sleep(2)
-        output = remote_conn.recv(5000)
-        print output
 
 if __name__ == "__main__":
-    sw_config = SwitchConfigurator()
-    sw_config.configure_switch()
+	sw_config = sw.SwitchConfigurator()
+	sw_config.configure_switch()

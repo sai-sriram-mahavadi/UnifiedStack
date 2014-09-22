f4cfaa95b689e74eacc23c1da5f29a2d53358bdc
Venkata Sai Sriram Mahavadi <vmahavad@cisco.com>
2014-09-22 20:10:16 +0530

    Polling functionality to check all nodes are up added into integrator.py

--- UnifiedStack/integrator.py
+++ UnifiedStack/integrator.py
@@ -16,9 +16,16 @@
 import sys
 import os
 import inspect
+import paramiko
+import time
+
 root_path = os.path.abspath(r"..")
 sys.path.append(root_path)
 
+# Hardcoded Values
+username = "root"
+password = "Cisco12345"
+MAX_TRIES = 5
 
 #from UnifiedStack.cimc import CIMC_Setup as cimc
 from UnifiedStack.masternode import cobbler_integrator as cobb
@@ -26,14 +33,17 @@ from UnifiedStack.packstack import Packstack_Setup as pst
 from UnifiedStack.netswitch import Switch_Setup as sw
 from UnifiedStack.cli import Shell_Interpretter as shi
 from UnifiedStack.cli import Console_Output as cli
+from UnifiedStack.config import Config_Parser
+# To Add
 #name, purpose(networker, compute), os -> name of system
 #system, rhel img (access.redhat)(http server), hostname port
 
+Config = Config_Parser.Config
 class Integrator:
     
     def configure_unifiedstack(self):
         console = cli.ConsoleOutput()
-	shi.ShellInterpretter.set_console(console)
+        shi.ShellInterpretter.set_console(console)
         console.cprint_header("UnifiedStack - Installer (Beta 1.0)")
         
         # Configuring Cobbler
@@ -64,6 +74,17 @@ class Integrator:
         cimc_config = cimc.CIMCConfigurator(console)
         cimc_config.configure_cimc()
         '''
+        count = 0
+        while not self.poll_all_nodes():
+            time.sleep(120)
+            if count < MAX_TRIES:
+                count += 1
+            else:
+                break
+        if not self.poll_all_nodes():
+            console.cprint("Not all systems could boot!!!")
+            exit(0)
+
         
         # Configuring Packstack
         console.cprint_progress_bar("Started Configuration of Packstack", 0)
@@ -74,8 +95,47 @@ class Integrator:
         console.cprint_progress_bar("Started Configuration of Switch", 0)
         sw_config = sw.SwitchConfigurator()
         sw_config.configure_switch(console)
-  
 
+
+    def poll_node(self, ipaddress, username, password):
+        remote_conn_pre = paramiko.SSHClient()
+        remote_conn_pre.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        try:
+            remote_conn_pre.connect(
+            ipaddress,
+            username=username,
+            password=password)
+        except socket.error:
+            return False
+        except Exception:
+            return False
+        return true
+    
+    def poll_all_nodes(self):
+        overall_poll_result = True        
+        for system in Config.get_systems_data():
+            # Calling the function to make the ssh connection
+            result = self.establish_connection(
+                                ipaddress=system.ip_address,
+                                username=username,
+                                password=password)
+            overall_poll_result = overall_poll_result and result
+            return overall_poll_result
+        
+    def test_poll(self):
+        count = 0
+        while not self.poll_all_nodes():
+            time.sleep(120)
+            if count < MAX_TRIES:
+                count += 1
+            else:
+                break
+        if self.poll_all_nodes(): pass
+                # Successful
+        else: pass
+                # UnSuccessful
+        
 if __name__ == "__main__":
     integrator = Integrator()
     integrator.configure_unifiedstack()
+    integrator.test_poll()

cf54816d89010e893f7f7ded5e51dd07e882dc04
Venkata Sai Sriram Mahavadi <vmahavad@cisco.com>
2014-09-29 16:29:48 +0530

    Formatted Switch_Setup in accordance to pep8 standards.

--- UnifiedStack/netswitch/Switch_Setup.py
+++ UnifiedStack/netswitch/Switch_Setup.py
@@ -1,10 +1,9 @@
-#import TS_Setup as TS
 
 
 class SwitchConfigurator:
-    
     # Returns SSH connection through which terminal needs to be invoked to send
     # a command and recieve it's output
+
     def establish_connection(self, ipaddress, username, password):
         remote_conn_pre = paramiko.SSHClient()
         # Automatically add untrusted hosts
@@ -42,28 +41,25 @@ class SwitchConfigurator:
         output = remote_conn.recv(5000)
 
     def configure_switch(self, console):
-        #sw3750_config = sw_3750.Switch3750Configurator()
-        #sw3750_config.configure_3750switch(console)
-        #console.cprint_progress_bar("Configured the 3750 switch", 50)
-        
         # Configuring 3750 Switch
         ip_address_9k = Config.get_switch_field("3750-ip-address")
         username_9k = Config.get_switch_field("3750-username")
         password_9k = Config.get_switch_field("3750-password")
-        self.configure_switch(ipaddress = ip_address_9k,
-                              username = username_9k,
-                              password = password_9,
-                              commands_file = 'netswitch/sw3750_commands.txt')
+        self.configure_switch(ipaddress=ip_address_9k,
+                              username=username_9k,
+                              password=password_9,
+                              commands_file='netswitch/sw3750_commands.txt')
+        console.cprint_progress_bar("Configured the 3750 switch", 50)
         
         # Configuring 9k Switch
         ip_address_9k = Config.get_switch_field("9k-ip-address")
         username_9k = Config.get_switch_field("9k-username")
         password_9k = Config.get_switch_field("9k-password")
-        self.configure_switch(ipaddress = ip_address_9k,
-                              username = username_9k,
-                              password = password_9,
-                              commands_file = 'netswitch/sw9k_commands.txt')
-        
+        self.configure_switch(ipaddress=ip_address_9k,
+                              username=username_9k,
+                              password=password_9,
+                              commands_file='netswitch/sw9k_commands.txt')
+
         console.cprint_progress_bar("Configured the N9K switch", 100)
 
 

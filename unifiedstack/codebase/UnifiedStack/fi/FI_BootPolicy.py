86c75bf6ecb1f3d7b329adf4272e5dee38f544fe
Aman Sinha <amansinh@cisco.com>
2014-11-25 16:07:20 +0530

    Added License

--- unifiedstack/codebase/UnifiedStack/fi/FI_BootPolicy.py
+++ unifiedstack/codebase/UnifiedStack/fi/FI_BootPolicy.py
@@ -1,3 +1,19 @@
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+
+# Configures Boot Policy.
+# PXE boot and local Disk storage configured.
+
+
 import UcsSdk as ucs
 from FI_Config_Base import FIConfiguratorBase
 
@@ -6,21 +22,21 @@ class FIBootPolicy(FIConfiguratorBase):
         handle = self.handle
         handle.StartTransaction()
         obj = handle.GetManagedObject(None, ucs.OrgOrg.ClassId(), {ucs.OrgOrg.DN:"org-root"})
-        mo = handle.AddManagedObject(obj, LsbootPolicy.ClassId(),
-     {LsbootPolicy.REBOOT_ON_UPDATE:"no",
-        LsbootPolicy.NAME:boot_policy_name,
-        LsbootPolicy.ENFORCE_VNIC_NAME:"yes",
-        LsbootPolicy.DN:"org-root/boot-policy-Boot_" + boot_policy_name ,
-        LsbootPolicy.DESCR:"Boot Policy for Cobbler"})
-        mo_1 = handle.AddManagedObject(mo, LsbootLan.ClassId(),
-       {LsbootLan.DN:"org-root/boot-policy-Boot_Local/lan",
-        LsbootLan.ORDER:"1", LsbootLan.PROT:"pxe"}, True)
-        mo_1_1 = handle.AddManagedObject(mo_1, LsbootLanImagePath.ClassId(),
-         {LsbootLanImagePath.DN:"org-root/boot-policy-" + boot_policy_name +"/lan/path-primary",
-        LsbootLanImagePath.VNIC_NAME:Vnic_name,
-        LsbootLanImagePath.TYPE:"primary"})
-        mo_3 = handle.AddManagedObject(mo, LsbootStorage.ClassId(),
-       {LsbootStorage.DN:"org-root/boot-policy-" + boot_policy_name + "/storage ",
-        LsbootStorage.ORDER:"2"})
-        mo_3_1 = handle.AddManagedObject(mo_3, LsbootLocalStorage.ClassId(),
-       {LsbootLocalStorage.DN:"org-root/boot-policy-" + boot_policy_name + "/storage/local-storage"})
+        mo = handle.AddManagedObject(obj, ucs.LsbootPolicy.ClassId(),
+     {ucs.LsbootPolicy.REBOOT_ON_UPDATE:"no",
+        ucs.LsbootPolicy.NAME:boot_policy_name,
+        ucs.LsbootPolicy.ENFORCE_VNIC_NAME:"yes",
+        ucs.LsbootPolicy.DN:"org-root/boot-policy-Boot_" + boot_policy_name ,
+        ucs.LsbootPolicy.DESCR:"Boot Policy for Cobbler"})
+        mo_1 = handle.AddManagedObject(mo, ucs.LsbootLan.ClassId(),
+       {ucs.LsbootLan.DN:"org-root/boot-policy-Boot_Local/lan",
+        ucs.LsbootLan.ORDER:"1", ucs.LsbootLan.PROT:"pxe"}, True)
+        mo_1_1 = handle.AddManagedObject(mo_1, ucs.LsbootLanImagePath.ClassId(),
+         {ucs.LsbootLanImagePath.DN:"org-root/boot-policy-" + boot_policy_name +"/lan/path-primary",
+        ucs.LsbootLanImagePath.VNIC_NAME:Vnic_name,
+        ucs.LsbootLanImagePath.TYPE:"primary"})
+        mo_3 = handle.AddManagedObject(mo, ucs.LsbootStorage.ClassId(),
+       {ucs.LsbootStorage.DN:"org-root/boot-policy-" + boot_policy_name + "/storage ",
+        ucs.LsbootStorage.ORDER:"2"})
+        mo_3_1 = handle.AddManagedObject(mo_3, ucs.LsbootLocalStorage.ClassId(),
+       {ucs.LsbootLocalStorage.DN:"org-root/boot-policy-" + boot_policy_name + "/storage/local-storage"})
